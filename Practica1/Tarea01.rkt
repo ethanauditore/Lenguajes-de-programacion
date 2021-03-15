c096b04b5762f6ac59a7591db0080fc0577cf222
ethanauditore <ethan_ortega@ciencias.unam.mx>
2021-03-15 15:19:59 -0600

    Ejercicios agregados

--- Practica1/Tarea01.rkt
+++ Practica1/Tarea01.rkt
@@ -1,1 +1,37 @@
 #lang plai
+
+;; Predicado que nos dice si un numero es negativo
+;; neg?: number -> Boolean
+(define (neg? a) ... )
+
+;; esPar?: numner -> boolean
+(define (esPar? n) ... )
+
+;; Procedimiento que nos devuelve el valor absoluto de un número
+;; absoluto: number -> number
+(define (absoluto n) ... )
+
+;; Procedimiento que calcula el area de un cono de base circular
+;; area-cono: number number -> number
+(define (area-cono d g) ... )
+
+;; Procedimiento que eleva el numero a, a la potencia b
+;; potencia: number number
+(define (potencia a b) ...)
+
+;; menores: number -> (listof number)
+(define (menores n) ... )
+
+;; suma-cuadrados: number -> number
+(define (suma-cuadrados n) ...)
+
+;; suma-cuadradosR: number -> number
+(define (suma-cuadradosR n) ...)
+
+;; Predicado que nos dice si un numero m es divisor de otro numero n
+;; divisor?: number number -> number
+(define (divisor? m n) ... )
+
+;; Procedimiento que determina el día de la semana correspondiente
+;; diaSemana: number -> string
+(define (daSemana n) ... )
