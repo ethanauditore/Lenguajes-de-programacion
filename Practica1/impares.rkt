0b3a37b37a8f7556faf048c7382eb6c82ad8f352
David <david1956@ciencias.unam.mx>
2021-03-17 02:37:55 -0600

    practica terminada

--- Practica1/impares.rkt
+++ Practica1/impares.rkt
@@ -1,5 +1,4 @@
 #lang plai
-
 ;; Predicado que nos dice si un numero es negativo
 ;; neg?: number -> Boolean
 ;(define (neg? a) ... )
@@ -8,28 +7,21 @@
     [(< a 0) #t]
     [else(> a 0)#f]))
 
-;; esPar?: numner -> boolean
-(define (esPar? n)
-  (= (modulo n 2) 0))
-
 ;; Procedimiento que nos devuelve el valor absoluto de un número
 ;; absoluto: number -> number
 ;(define (absoluto n) ... )
+
 (define (absoluto n)
   (cond
     [(< n 0) (* -1 n)]
     [(= n 0) 0]
     [(> n 0) n]))
 
-;; Procedimiento que calcula el area de un cono de base circular
-;; area-cono: number number -> number
-(define (area-cono d g)
-  (define r (/ d 2))
-  (+ (* pi r g) (* pi (expt r 2))))
-
 ;; Procedimiento que eleva el numero a, a la potencia b
 ;; potencia: number number
 ;(define (potencia a b) ...)
+
+
 
 (define (potencia a b)
   (cond
@@ -41,23 +33,13 @@
     )
   )
 
-;; menores: number -> (listof number)
-(define (menores n)
-  (cond
-    [(= n 0) (list 0)]
-    [else (append (menores (sub1 n)) (list n))]))
+
 
 ;; suma-cuadrados: number -> number
 ;(define (suma-cuadrados n) ...)
 
 (define (suma-cuadrados n)
   (/ (* n (+ n 1) (+ (* 2 n) 1)) 6))
-
-;; suma-cuadradosR: number -> number
-(define (suma-cuadradosR n)
-  (cond
-    [(= n 0) 0]
-    [else (+ (expt n 2) (suma-cuadradosR (sub1 n)))]))
 
 ;; Predicado que nos dice si un numero m es divisor de otro numero n
 ;; divisor?: number number -> number
@@ -68,16 +50,3 @@
     [(=(modulo n m)0) #t]
     [else
          #f]))
-
-;; Procedimiento que determina el día de la semana correspondiente
-;; diaSemana: number -> string
-(define (diaSemana n)
-  (define m (modulo n 7))
-  (cond
-    [(= m 0) "Lunes"]
-    [(= m 1) "Martes"]
-    [(= m 2) "Miercoles"]
-    [(= m 3) "Jueves"]
-    [(= m 4) "Viernes"]
-    [(= m 5) "Sabado"]
-    [(= m 6) "Domingo"]))
